14baf9a217c292ab00cb0e5d16d25e916d7fd3be
云步青烟 <echoall@outlook.com>
2023-04-26 09:30:02 +0800

    c

--- js/common.js
+++ js/common.js
@@ -3,11 +3,30 @@ $(document).ready(function() {
 
   var headerOverlay = $(".header__overlay"),
     menuOpenIcon = $(".nav__icon-menu"),
-    menuCloseIcon = $(".nav__close"),
-    menuList = $(".main-nav__box"),
+    menuCloseIcon = $(".ion-md-close"),
+    menuList = $(".main-nav"),
     searchOpenIcon = $(".nav__icon-search"),
     searchCloseIcon = $(".search__close"),
     searchBox = $(".search");
+
+  /* =======================
+  // Hide Header
+  ======================= */
+  header();
+
+  function header() {
+    var initialScroll;
+    $(window).scroll(function () {
+      var scroll = $(this).scrollTop();
+      if (scroll > initialScroll && initialScroll > 100) {
+        $('.header').addClass('is-hide');
+      } else {
+        $('.header').removeClass('is-hide');
+      }
+      initialScroll = scroll;
+    });
+  }
+
 
   /* =======================
   // Menu and Search
@@ -52,10 +71,52 @@ $(document).ready(function() {
   }
 
   /* =======================
+  // Slick Slider
+  ======================= */
+  $('.slider__box').slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 8000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    dots: false,
+    centerMode: true,
+    adaptiveHeight: true,
+    fade: true,
+    cssEase: 'linear',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          dots: false,
+          arrows: true
+        }
+      }
+    ]
+  });
+
+
+  /* =======================
+  // Masonry Grid Layout
+  ======================= */
+  var $grid = $('.grid').masonry({
+    itemSelector: '.grid__post',
+    percentPosition: true
+  });
+
+  $grid.imagesLoaded().progress(function () {
+    $grid.masonry('layout');
+  });
+
+
+  /* =======================
   // Responsive Videos
   ======================= */
   $(".post__content, .page__content").fitVids({
-    customSelector: ['iframe[src*="ted.com"]']
+    customSelector: ['iframe[src*="ted.com"]', 'iframe[src*="facebook.com"]']
   });
 
   /* =======================
@@ -63,6 +124,26 @@ $(document).ready(function() {
   ======================= */
   $(".page img, .post img").attr("data-action", "zoom");
   $(".page a img, .post a img").removeAttr("data-action", "zoom");
+
+
+  /* =======================
+  // Instagram Feed
+  ======================= */
+  var userId = $('#instafeed').attr('data-userId');
+  var accessToken = $('#instafeed').attr('data-accessToken');
+  var instagramFeed = new Instafeed({
+    get: 'user',
+    limit: 4,
+    resolution: 'standard_resolution',
+    userId: userId,
+    accessToken: accessToken,
+    template:
+      '<li class="instagram-item"><a href="{{link}}" class="instagram-link" aria-label="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}"></a></li>'
+  });
+
+  if ($('#instafeed').length) {
+    instagramFeed.run();
+  }
 
   /* =======================
   // Scroll Top Button
@@ -80,10 +161,5 @@ $(document).ready(function() {
     }
   });
 
-  /* =============================
-  // WOW Animation When You Scroll
-  ============================= */
-  $(".page img, .post img").addClass("wow fadeIn");
-  new WOW().init();
 
 });
